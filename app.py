88626e9c68fc4252b3898f8ed4469bb437591209
Sohom Pal <sohom377@gmail.com>
2023-09-20 17:25:10 +0530

    working well

--- app.py
+++ app.py
@@ -4,21 +4,23 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.encoders import jsonable_encoder
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import QAGenerationChain, RetrievalQA
-from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import QAGenerationChain
 from langchain.text_splitter import TokenTextSplitter
-from langchain.document_loaders import PyPDFLoader
 from langchain.docstore.document import Document
+from langchain.document_loaders import PyPDFLoader
 from langchain.prompts import PromptTemplate
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import RetrievalQA
 import os
 import json
 import time
 import uvicorn
 import aiofiles
-import csv
 from PyPDF2 import PdfReader
+import csv
+from dotenv import load_dotenv  # Import python-dotenv library
 
 app = FastAPI()
 
@@ -26,7 +28,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-os.environ["OPENAI_API_KEY"] = ""
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the OpenAI API key from the environment
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+# Use the API key in your code
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
 
 def count_pdf_pages(pdf_path):
     try:
@@ -35,8 +45,10 @@ def count_pdf_pages(pdf_path):
     except Exception as e:
         print("Error:", e)
         return None
-    
+
 def file_processing(file_path):
+
+    # Load data from PDF
     loader = PyPDFLoader(file_path)
     data = loader.load()
 
@@ -44,24 +56,25 @@ def file_processing(file_path):
 
     for page in data:
         question_gen += page.page_content
-
-    splitter_gues_gen = TokenTextSplitter(
-        model = "gpt-3,5-turbo" , 
+        
+    splitter_ques_gen = TokenTextSplitter(
+        model_name = 'gpt-3.5-turbo',
         chunk_size = 10000,
         chunk_overlap = 200
     )
 
-    chunks_ques_gen = splitter_gues_gen.split_text(question_gen)
+    chunks_ques_gen = splitter_ques_gen.split_text(question_gen)
 
     document_ques_gen = [Document(page_content=t) for t in chunks_ques_gen]
 
     splitter_ans_gen = TokenTextSplitter(
-        model = "gpt-3.5-turbo",
-        chunk_size = 10000,
-        chunk_overlap = 200
+        model_name = 'gpt-3.5-turbo',
+        chunk_size = 1000,
+        chunk_overlap = 100
     )
 
-    document_answer_gen = splitter_ans_gen.split_text(
+
+    document_answer_gen = splitter_ans_gen.split_documents(
         document_ques_gen
     )
 
@@ -76,73 +89,81 @@ def llm_pipeline(file_path):
         model = "gpt-3.5-turbo"
     )
 
-    prompt_template = """"
-    You are an expert at creating questions based on study materials and reference guides.
-    Your goal is to prepare a student or teacher for their exam and tests.
+    prompt_template = """
+    You are an expert at creating questions based on coding materials and documentation.
+    Your goal is to prepare a coder or programmer for their exam and coding tests.
     You do this by asking questions about the text below:
-    -----------------
+
+    ------------
     {text}
-    -----------------
-    Create questions that will prepare the student or teacher for their tests.
+    ------------
+
+    Create questions that will prepare the coders or programmers for their tests.
     Make sure not to lose any important information.
-    QUESTIONS:  
+
+    QUESTIONS:
     """
 
-    PROMPT_QUESTIONS = PromptTemplate(
-        template = prompt_template,
-        input_variables = ["text"]
-    )
+    PROMPT_QUESTIONS = PromptTemplate(template=prompt_template, input_variables=["text"])
 
     refine_template = ("""
-    You are an expert at creating practice questions based on study materials and reference guides.
-    Your goal is to help a student or teacher for their exams and tests.
+    You are an expert at creating practice questions based on coding material and documentation.
+    Your goal is to help a coder or programmer prepare for a coding test.
     We have received some practice questions to a certain extent: {existing_answer}.
     We have the option to refine the existing questions or add new ones.
     (only if necessary) with some more context below.
-    -----------------
+    ------------
     {text}
-    -----------------
-    Gievn the new context, refine the original questions in English.
+    ------------
+
+    Given the new context, refine the original questions in English.
     If the context is not helpful, please provide the original questions.
     QUESTIONS:
     """
     )
 
     REFINE_PROMPT_QUESTIONS = PromptTemplate(
-        input_variables = ["existing_answer", "text"],
-        template = refine_template
+        input_variables=["existing_answer", "text"],
+        template=refine_template,
     )
 
-    ques_gen_chain = load_summarize_chain(
-        llm = llm_ques_gen_pipeline,
-        chain_type= "refine",
-        verbose = True,
-        question_prompt =  PROMPT_QUESTIONS,
-        refine_prompt = REFINE_PROMPT_QUESTIONS
+    ques_gen_chain = load_summarize_chain(llm = llm_ques_gen_pipeline, 
+                                            chain_type = "refine", 
+                                            verbose = True, 
+                                            question_prompt=PROMPT_QUESTIONS, 
+                                            refine_prompt=REFINE_PROMPT_QUESTIONS)
 
-    )
-    ques = ques_gen_chain.run(document_answer_gen)
+    ques = ques_gen_chain.run(document_ques_gen)
 
     embeddings = OpenAIEmbeddings()
 
     vector_store = FAISS.from_documents(document_answer_gen, embeddings)
 
-    llm_answer_gen = ChatOpenAI(
-        temperature = 0.1,
-        model = "gpt-3.5-turbo"
-    )
-
-    ques_list = ques.split("\n")
+    llm_answer_gen = ChatOpenAI(temperature=0.1, model="gpt-3.5-turbo")
 
     ques_list = ques.split("\n")
     filtered_ques_list = [element for element in ques_list if element.endswith('?') or element.endswith('.')]
 
-    answer_generation_chain = RetrievalQA.from_chain_type(
-        llm=llm_answer_gen, 
-        chain_type="stuff", 
-        retriever=vector_store.as_retriever())
+    answer_generation_chain = RetrievalQA.from_chain_type(llm=llm_answer_gen, 
+                                                chain_type="stuff", 
+                                                retriever=vector_store.as_retriever())
 
     return answer_generation_chain, filtered_ques_list
+
+
+
+
+# Answer each question and save to a file
+# for question in question_list:
+#     print("Question: ", question)
+#     answer = answer_gen_chain.run(question)
+#     print("Answer: ", answer)
+#     print("--------------------------------------------------\\n\\n")
+#     # Save answer to file
+#     with open("answers.txt", "a") as f:
+#         f.write("Question: " + question + "\\n")
+#         f.write("Answer: " + answer + "\\n")
+#         f.write("--------------------------------------------------\\n\\n")
 
 def get_csv (file_path):
     answer_generation_chain, ques_list = llm_pipeline(file_path)
