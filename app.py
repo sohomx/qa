9fb9d6f5673fcf7c187f675612c8f5b43d981791
Sohom Pal <sohom377@gmail.com>
2023-09-20 14:37:08 +0530

    wip

--- app.py
+++ app.py
@@ -143,3 +143,54 @@ def llm_pipeline(file_path):
         retriever=vector_store.as_retriever())
 
     return answer_generation_chain, filtered_ques_list
+
+def get_csv (file_path):
+    answer_generation_chain, ques_list = llm_pipeline(file_path)
+    base_folder = 'static/output/'
+    if not os.path.isdir(base_folder):
+        os.mkdir(base_folder)
+    output_file = base_folder+"QA.csv"
+    with open(output_file, "w", newline="", encoding="utf-8") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["Question", "Answer"])  # Writing the header row
+
+        for question in ques_list:
+            print("Question: ", question)
+            answer = answer_generation_chain.run(question)
+            print("Answer: ", answer)
+            print("--------------------------------------------------\n\n")
+
+            # Save answer to CSV file
+            csv_writer.writerow([question, answer])
+    return output_file
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/upload")
+async def chat(request: Request, pdf_file: bytes = File(), filename: str = Form(...)):
+    base_folder = 'static/docs/'
+    if not os.path.isdir(base_folder):
+        os.mkdir(base_folder)
+    pdf_filename = os.path.join(base_folder, filename)
+
+    async with aiofiles.open(pdf_filename, 'wb') as f:
+        await f.write(pdf_file)
+    # page_count = count_pdf_pages(pdf_filename)
+    # if page_count > 5:
+    #     return Response(jsonable_encoder(json.dumps({"msg": 'error'})))
+    response_data = jsonable_encoder(json.dumps({"msg": 'success',"pdf_filename": pdf_filename}))
+    res = Response(response_data)
+    return res
+
+
+@app.post("/analyze")
+async def chat(request: Request, pdf_filename: str = Form(...)):
+    output_file = get_csv(pdf_filename)
+    response_data = jsonable_encoder(json.dumps({"output_file": output_file}))
+    res = Response(response_data)
+    return res
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host='0.0.0.0', port=8000, reload=True)
