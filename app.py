58a1221c119ec4af13668e3d9d4a5d6c98a8a416
Sohom Pal <sohom377@gmail.com>
2023-09-20 14:18:39 +0530

    llm pipeline

--- app.py
+++ app.py
@@ -67,3 +67,79 @@ def file_processing(file_path):
 
     return document_ques_gen, document_answer_gen
 
+def llm_pipeline(file_path):
+
+    document_ques_gen, document_answer_gen = file_processing(file_path)
+
+    llm_ques_gen_pipeline = ChatOpenAI(
+        temperature = 0.3,
+        model = "gpt-3.5-turbo"
+    )
+
+    prompt_template = """"
+    You are an expert at creating questions based on study materials and reference guides.
+    Your goal is to prepare a student or teacher for their exam and tests.
+    You do this by asking questions about the text below:
+    -----------------
+    {text}
+    -----------------
+    Create questions that will prepare the student or teacher for their tests.
+    Make sure not to lose any important information.
+    QUESTIONS:  
+    """
+
+    PROMPT_QUESTIONS = PromptTemplate(
+        template = prompt_template,
+        input_variables = ["text"]
+    )
+
+    refine_template = ("""
+    You are an expert at creating practice questions based on study materials and reference guides.
+    Your goal is to help a student or teacher for their exams and tests.
+    We have received some practice questions to a certain extent: {existing_answer}.
+    We have the option to refine the existing questions or add new ones.
+    (only if necessary) with some more context below.
+    -----------------
+    {text}
+    -----------------
+    Gievn the new context, refine the original questions in English.
+    If the context is not helpful, please provide the original questions.
+    QUESTIONS:
+    """
+    )
+
+    REFINE_PROMPT_QUESTIONS = PromptTemplate(
+        input_variables = ["existing_answer", "text"],
+        template = refine_template
+    )
+
+    ques_gen_chain = load_summarize_chain(
+        llm = llm_ques_gen_pipeline,
+        chain_type= "refine",
+        verbose = True,
+        question_prompt =  PROMPT_QUESTIONS,
+        refine_prompt = REFINE_PROMPT_QUESTIONS
+
+    )
+    ques = ques_gen_chain.run(document_answer_gen)
+
+    embeddings = OpenAIEmbeddings()
+
+    vector_store = FAISS.from_documents(document_answer_gen, embeddings)
+
+    llm_answer_gen = ChatOpenAI(
+        temperature = 0.1,
+        model = "gpt-3.5-turbo"
+    )
+
+    ques_list = ques.split("\n")
+
+    ques_list = ques.split("\n")
+    filtered_ques_list = [element for element in ques_list if element.endswith('?') or element.endswith('.')]
+
+    answer_generation_chain = RetrievalQA.from_chain_type(
+        llm=llm_answer_gen, 
+        chain_type="stuff", 
+        retriever=vector_store.as_retriever())
+
+    return answer_generation_chain, filtered_ques_list
